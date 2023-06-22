b8578d76fd761bbce6ddfc84fbae033160f3c2ea
Ruby Chattrath <122198061+rubykaurchattrath@users.noreply.github.com>
2023-06-22 13:07:02 -0700

    Update buyLotsOfFruit.py

--- buyLotsOfFruit.py
+++ buyLotsOfFruit.py
@@ -22,6 +22,7 @@ the script should produce the output:
 
 Cost of [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)] is 12.25
 """
+
 from __future__ import print_function
 
 fruitPrices = {'apples': 2.00, 'oranges': 1.50, 'pears': 1.75,
@@ -35,9 +36,13 @@ def buyLotsOfFruit(orderList):
     Returns cost of order
     """
     totalCost = 0.0
-    "*** YOUR CODE HERE ***"
+    for fruit, pounds in orderList:
+        if fruit in fruitPrices:
+            totalCost += fruitPrices[fruit] * pounds
+        else:
+            print("Error: Fruit '{}' is not available.".format(fruit))
+            return None
     return totalCost
-
 
 # Main Method
 if __name__ == '__main__':
